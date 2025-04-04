5e111eb5b40b36b1c1a23f3a0a8ac50728879e2e
Shelly B <sbaldwin734@gmail.com>
2025-04-03 23:22:50 -0400

    build config updates

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,6 +1,4 @@
-// Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
-// Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
-// stop passing in HOST, so we can remove this workaround after the next major release.
+// remix.config.cjs
 if (
   process.env.HOST &&
   (!process.env.SHOPIFY_APP_URL ||
@@ -10,11 +8,11 @@ if (
   delete process.env.HOST;
 }
 
-/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
   serverModuleFormat: "cjs",
+  serverBuildTarget: "node-cjs",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
   future: {},
 };
